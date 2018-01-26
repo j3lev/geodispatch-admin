dea317d58f02f72423ae6f3d8449b470069e581a
Joseph Levenson <josephlevenson@Josephs-MBP.home>
2018-01-26 18:43:21 -0500

    Added mapbox with viewport changes

--- src/components/MapboxContainer.jsx
+++ src/components/MapboxContainer.jsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
 
 export class MapboxContainer extends Component {
-    render () {
+    constructor() {
+        super();
+        this.token = 'pk.eyJ1IjoibGV2ZW5zb25qIiwiYSI6ImNqY3c2emk4dDBkaTcyeG4yNzVtNXpsanMifQ.MhkQq2XlVvahW3vOAyzXhg';
+        this.state = {
+            latitude: 37.7577,
+            longitude: -122.4376,
+            zoom: 8 
+        };
+    }
+
+    render() {
         return (
             <div>
-                <p>This is the mapbox container</p>
+                <ReactMapGL { ...this.state }
+                    dragPan
+                    mapboxApiAccessToken={this.token}
+                    width={500} 
+                    height={500}
+                    onViewportChange={(viewport) => {
+                        this.setState(viewport);
+                    }} />
             </div>
         )
     }
